25c46be39df2730dec7f0a4f620bb918fc295e54
Gederson <gedersongustavogbot@ufps.edu.co>
2023-05-25 17:24:09 -0500

    Control de errores cliente: Advice

--- src/main/java/com/devsu/apirest/infrastructure/exceptionhandler/ControllerAdvisor.java
+++ src/main/java/com/devsu/apirest/infrastructure/exceptionhandler/ControllerAdvisor.java
@@ -3,10 +3,13 @@ package com.devsu.apirest.infrastructure.exceptionhandler;
 import com.devsu.apirest.infrastructure.exception.NoDataFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -19,6 +22,19 @@ public class ControllerAdvisor {
             NoDataFoundException ignoredNoDataFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Collections.singletonMap(MESSAGE, ExceptionResponse.NO_DATA_FOUND.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleClienteArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errors);
     }
     
 }
